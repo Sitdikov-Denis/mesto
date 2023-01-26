f003c04f4d2b00701aafc376024aea05ff759799
Sitdikov-Denis <denis.sitdikov95@yandex.ru>
2023-01-26 13:50:00 +0300

    add initialCards

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 let closeButton = document.querySelector('.profile-form__toggle');
 let editButton = document.querySelector('.profile__edit-button');
+console.log(editButton)
 let popup = document.querySelector('.profile-form');
 
 let editForm = document.querySelector('.profile-form__form');
 
-let currencyName = document.querySelector('.profile__name');
-let currencyDescription = document.querySelector('.profile__description');
+let currentName = document.querySelector('.profile__name');
+let currentDescription = document.querySelector('.profile__description');
 
-let fieldName = document.getElementById('profileName');
-let fieldDescription = document.getElementById('profileDescription');
+let fieldName = document.querySelector('#profileName');
+let fieldDescription = document.querySelector('#profileDescription');
 
 function popupOpen() {
   popup.classList.add('profile-form_active');
-  fieldName.value = currencyName.textContent;
-  fieldDescription.value = currencyDescription.textContent;
+  fieldName.value = currentName.textContent;
+  fieldDescription.value = currentDescription.textContent;
 }
 
 function popupClose() {
@@ -22,13 +23,49 @@ function popupClose() {
 
 function changeName(evt) {
   evt.preventDefault();
-  currencyName.textContent = fieldName.value;
-  currencyDescription.textContent = fieldDescription.value;
+  currentName.textContent = fieldName.value;
+  currentDescription.textContent = fieldDescription.value;
   popupClose();
 }
 
+const elementsContainer = document.querySelector('.elements');
+const elementTemplate = document.querySelector('#element-template').content;
+
+const initialCards = [
+  {
+    name: 'Собакин',
+    link: './images/elements/dog.jpg'
+  },
+  {
+    name: 'Осень',
+    link: './images/elements/autumn.jpg'
+  },
+  {
+    name: 'Норвегия',
+    link: './images/elements/norway.jpg'
+  },
+  {
+    name: 'Котики',
+    link: './images/elements/cat.jpg'
+  },
+  {
+    name: 'Горы',
+    link: './images/elements/mountain.jpg'
+  },
+  {
+    name: 'Северное сияние',
+    link: './images/elements/nord.jpg'
+  }
+];
+
+initialCards.forEach((element) => {
+  const elementCard = elementTemplate.querySelector('.element').cloneNode(true);
+  elementCard.querySelector('.element__title').textContent = element.name;
+  elementCard.querySelector('.element__photo').src = element.link;
+  elementCard.querySelector('.element__photo').alt = element.name;
+  elementsContainer.append(elementCard);
+})
 
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
 editForm.addEventListener('submit', changeName);
-
